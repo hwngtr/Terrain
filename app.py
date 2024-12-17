3d396b4f730b52ba339125cc16756db06a4fd76e
TanHung <144088836+hwngtr@users.noreply.github.com>
2024-12-17 17:57:49 +0000

    test2

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     # Display image
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.image(image, caption='Uploaded Image', use_container_width=True)
     
     # Make prediction
     predictions, prob, class_idx = predict(image, model)
